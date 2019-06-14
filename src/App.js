8a8a6bcb5f137ef998c9bd097a519eff17cfaf07
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-06-14 13:52:02 -0700

    Testing branch deployment.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - This is an update on the testing split branch.
         </a>
       </header>
     </div>
